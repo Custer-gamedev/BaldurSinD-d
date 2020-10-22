6e4dfdece2398cba507fefb305419788e63d72da
Brage <brage@reboot.no>
2020-10-22 18:20:35 +0200

    Orbs som har farger

--- Assets/Enemies/Scripts/Stats, Misc/Souls.cs
+++ Assets/Enemies/Scripts/Stats, Misc/Souls.cs
@@ -4,35 +4,44 @@ using UnityEngine;
 public class Souls : MonoBehaviour
 {
 
-    private Transform destination;
-    void Awake()
-    {
-        SpawnSoul();
-    }
+	private Transform destination;
+	private ParticleSystem ps;
+	void Awake()
+	{
+		ps = GetComponent<ParticleSystem>();
+		SpawnSoul();
+	}
 
-    void SpawnSoul()
-    {
-        int rand = Random.Range(0, 2);
-        print(rand);
-        if(rand == 0)
-        {
-            GameObject bossRoom = GameObject.FindGameObjectWithTag("BossRoom");
-            //GetComponent<ParticleSystem>().startColor = Color.red;
-            destination = bossRoom.transform;
-            Destroy(gameObject, 3f);
-        }
-        if(rand == 1)
-        {
-            GameObject treasureRoom = GameObject.FindGameObjectWithTag("TreasureRoom");
-            //GetComponent<ParticleSystem>().startColor = Color.yellow;
-            destination = treasureRoom.transform;
+	void SpawnSoul()
+	{
+		int rand = Random.Range(0, 2);
+		print(rand);
+		if (rand == 0)
+		{
+			GameObject bossRoom = GameObject.FindGameObjectWithTag("BossRoom");
+			destination = bossRoom.transform;
+			var main = ps.main;
+			Color bossColor;
 
-            Destroy(gameObject, 3f);
-        }
-    }
-    void Update()
-    {
-            transform.position = Vector3.Lerp(transform.position, destination.transform.position, .3f * Time.deltaTime);
-     
-    }
+			ColorUtility.TryParseHtmlString("#D42F25", out bossColor);
+			main.startColor = bossColor;
+			Destroy(gameObject, 3f);
+		}
+		if (rand == 1)
+		{
+			GameObject treasureRoom = GameObject.FindGameObjectWithTag("TreasureRoom");
+			destination = treasureRoom.transform;
+			var main = ps.main;
+			Color treasureColor;
+
+			ColorUtility.TryParseHtmlString("#FFF400", out treasureColor);
+			main.startColor = treasureColor;
+			Destroy(gameObject, 3f);
+		}
+	}
+	void Update()
+	{
+		transform.position = Vector3.Lerp(transform.position, destination.transform.position, .3f * Time.deltaTime);
+
+	}
 }
--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 
 public class cameraFollow : MonoBehaviour
 {
-    public Transform Target;
-    public float Smoothening;
-    public Vector3 Offset;
-    void Start()
-    {
-        
-    }
+	public Transform Target;
+	public float Smoothening;
+	public Vector3 Offset;
+	void Start()
+	{
 
-    private void FixedUpdate()
-    {        
-        Vector3 endPos = Target.position + Offset;
-        Vector3 smoothPos = Vector3.Lerp(transform.position, endPos, Smoothening);
-        transform.position = smoothPos;
-    }
+	}
+
+	private void FixedUpdate()
+	{
+		Vector3 endPos = Target.position + Offset;
+		Vector3 smoothPos = Vector3.Lerp(transform.position, endPos, Smoothening);
+		transform.position = smoothPos;
+	}
 }
