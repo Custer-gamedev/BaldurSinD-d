834bb097935a792c36be5e69eadc6b97442e7171
Emil Steinsholt <emil.steinsholt@gmail.com>
2020-10-29 11:53:52 +0100

    boss nesten ferdig, ny tag

---
+++ Assets/Prellmoll Test/Dwarf_Boss/Boss_Dwarf_Stats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Dwarf_Stats : MonoBehaviour
+{
+    public bool phase2 = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
