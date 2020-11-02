88c6d5d1ceb1ab071e9691b6e962c5bd8672f902
Brage <brage@reboot.no>
2020-11-02 18:11:48 +0100

    Små bug fixes

--- Assets/Enemies/Scripts/Melee/Melee_Damage.cs
+++ Assets/Enemies/Scripts/Melee/Melee_Damage.cs
@@ -3,43 +3,44 @@ using UnityEngine.AI;
 
 public class Melee_Damage : MonoBehaviour
 {
-    public EnemyStats eStats;
-    public float damage;
-    public GameObject player;
-    public float waitTime;
-    private float attackTime;
-    private void Awake()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        eStats = GetComponentInParent<EnemyStats>();
-        attackTime = waitTime;
-        damage = eStats.damage;
-        GetComponentInParent<NavMeshAgent>().isStopped = false;
+	public EnemyStats eStats;
+	public float damage;
+	public GameObject player;
+	public float waitTime;
+	private float attackTime;
+	private void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		eStats = GetComponentInParent<EnemyStats>();
+		attackTime = waitTime;
+		damage = eStats.damage;
+		GetComponentInParent<NavMeshAgent>().isStopped = false;
 
-    }
-    void Update()
-    {
-        GetComponentInParent<NavMeshAgent>().SetDestination(player.transform.position);
-    }
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            GetComponentInParent<NavMeshAgent>().isStopped = true;
-            if (attackTime <= 0)
-            {
-                other.GetComponent<PlayerStats>().TakeDamage(damage);
-                attackTime = waitTime;
-            }
-            else
-                attackTime -= Time.deltaTime;
-        }
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            attackTime = waitTime;
-        }
-    }
+	}
+	void Update()
+	{
+		GetComponentInParent<NavMeshAgent>().SetDestination(player.transform.position);
+	}
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			GetComponentInParent<NavMeshAgent>().isStopped = true;
+			if (attackTime <= 0)
+			{
+				other.GetComponent<PlayerStats>().TakeDamage(damage);
+				attackTime = waitTime;
+			}
+			else
+				attackTime -= Time.deltaTime;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			GetComponentInParent<NavMeshAgent>().isStopped = false;
+			attackTime = waitTime;
+		}
+	}
 }
