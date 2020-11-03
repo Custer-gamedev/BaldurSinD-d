22e7122f233816084d085d560521dbf487e1f28b
Brage <brage@reboot.no>
2020-11-03 17:15:27 +0100

    noen endringer i ting

---
+++ Assets/SaveData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Configuration;
+
+public static class SaveData
+{
+    static string Path()
+    {
+        return Application.dataPath + "SavaData.json";
+    }
+    
+   public static void Save(DataToSave saveD)
+    {
+        var json = JsonUtility.ToJson(saveD);
+        File.WriteAllText(Path(), json);
+    }
+    
+}
