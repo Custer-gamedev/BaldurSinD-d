e11656e05ea66ba844538e6d1d7b9355a2a54ae3
Brage <brage@reboot.no>
2020-11-10 12:22:54 +0100

    Dørene er lagt inn med åpen og lukket animasjon. Mulig at jeg må scale det ned (TEST)

--- Assets/ROOMS/Doors.cs
+++ Assets/ROOMS/Doors.cs
@@ -2,41 +2,49 @@
 
 public class Doors : MonoBehaviour
 {
-    public Transform doorSpawn;
-    public GameObject playerCam;
-    public GameObject player;
-    public Transform oC;
-    public static bool locked;
+	public Transform doorSpawn;
+	public GameObject playerCam;
+	public GameObject player;
+	public Animator doorAnim;
+	public Transform oC;
+	public static bool locked;
 
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        doorSpawn = this.gameObject.transform.GetChild(0);
-        playerCam = GameObject.Find("Main Camera");
-    }
-    private void Update()
-    {
-        oC = gameObject.GetComponentInChildren<SetCam>().otherCamPos;
+	private void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		doorSpawn = this.gameObject.transform.GetChild(0);
+		playerCam = GameObject.Find("Main Camera");
+		doorAnim = GetComponentInParent<Animator>();
+	}
+	private void Update()
+	{
+		oC = gameObject.GetComponentInChildren<SetCam>().otherCamPos;
+		if (locked == true)
+		{
+			doorAnim.SetBool("IsOpen", false);
+		}
+		else if (locked == false)
+			doorAnim.SetBool("IsOpen", true);
 
-    }
-    private void OnTriggerEnter(Collider other)
-    {
+	}
+	private void OnTriggerEnter(Collider other)
+	{
 
-        if (locked == false)
-        {
+		if (locked == false)
+		{
 
-            if (other.transform.name == "Player")
-            {
-                ChangeRoom();
-            }
-        }
-    }
+			if (other.transform.name == "Player")
+			{
+				ChangeRoom();
+			}
+		}
+	}
 
-    void ChangeRoom()
-    {
-        player.transform.position = doorSpawn.transform.position;
-        playerCam.transform.position = oC.position;
-    }
+	void ChangeRoom()
+	{
+		player.transform.position = doorSpawn.transform.position;
+		playerCam.transform.position = oC.position;
+	}
 
 
 }
