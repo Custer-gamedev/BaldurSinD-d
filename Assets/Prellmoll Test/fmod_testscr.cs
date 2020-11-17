37b6b316643f82535365aad21163aae7d4dcfb92
Emil Steinsholt <emil.steinsholt@gmail.com>
2020-11-17 14:07:20 +0100

    en hel haug med fuck for version control på fmod og legge inn lyder
    driten funker ikke som den skal ! ! ! ! faen

---
+++ Assets/Prellmoll Test/fmod_testscr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class fmod_testscr : MonoBehaviour
+{
+    float hp;
+    float intensity;
+
+    
+    void Start()
+    {   
+        
+        FMODUnity.RuntimeManager.PlayOneShot("event:/MUS/MUS_F1", gameObject.transform.position);
+    }
+    
+    void Update()
+    {
+     if(Input.GetKeyDown(KeyCode.Space)){
+         FMODUnity.RuntimeManager.PlayOneShot("event:/Hermod/ATK_1", gameObject.transform.position);
+         
+     }   
+    }
+}
