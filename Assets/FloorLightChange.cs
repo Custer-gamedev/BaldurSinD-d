f9bfd4bab7a144d994a4b447173d0bd6370894a7
Fredrik <Fredrik_h@live.com>
2020-11-30 18:45:06 +0100

    Floor 2 har fixed light, øksa bruker riktig model, boss rommene er cool

---
+++ Assets/FloorLightChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FloorLightChange : MonoBehaviour
+{
+    public string sNname;
+    public Color lightcolor;
+    void Start()
+    {
+       sNname = SceneManager.GetActiveScene().name.ToString();
+        if (sNname == "FloorTwo")
+        {
+            GetComponent<Light>().color = lightcolor;
+        }
+    }
+}
