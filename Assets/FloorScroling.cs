076872034be87a369e719ef6ec779832e13b8646
Fredrik <Fredrik_h@live.com>
2020-11-30 18:15:15 +0100

    Made the levels nice, skal lage floor 2 og mere lighting (safe build)

---
+++ Assets/FloorScroling.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScroling : MonoBehaviour
+{
+    public float xRand, yRand;
+    public Material rend;
+
+    void Start()
+    {
+         xRand = Random.Range(.1f, 1);
+         yRand = Random.Range(.1f, 1);
+
+        rend = GetComponent<MeshRenderer>().material;
+
+        rend.SetTextureOffset("Base Textur", new Vector2(xRand,yRand));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
