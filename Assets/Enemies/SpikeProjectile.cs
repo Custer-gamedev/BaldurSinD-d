4fba9c05ef529c674c31dfa107ea61da6f2eddf2
Brage <brage@reboot.no>
2020-12-02 23:59:43 +0100

    små fikses + han er plassert riktig (kommer til å få errors med IceTips

---
+++ Assets/Enemies/SpikeProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeProjectile : MonoBehaviour
+{
+	private LineRenderer lr;
+	public LayerMask groundMask;
+	public Transform tip;
+	private void Start()
+	{
+		lr = GetComponent<LineRenderer>();
+	}
+	private void Update()
+	{
+		lr.SetPosition(0, tip.transform.position);
+		RaycastHit hit;
+		if (Physics.Raycast(tip.transform.position, tip.transform.up, out hit, 30, groundMask))
+		{
+			lr.SetPosition(1, hit.transform.position);
+			print(hit.transform.position);
+		}
+	}
+	private void OnDrawGizmos()
+	{
+		Gizmos.DrawRay(tip.transform.position, tip.transform.up * 20);
+	}
+}
